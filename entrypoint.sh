853b44e097ae32e133c6d903ccc18b371f9b3595
crypticmeta <crypticmeta@coderixx.com>
2024-06-25 14:05:09 +0530

    revert entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,6 @@ service postgresql start
 # Set PostgreSQL password
 su - postgres -c "psql -c \"ALTER USER postgres PASSWORD 'postgres';\""
 
-# Check if the ord binary exists in the mounted volume, if not copy it from the image
-if [ ! -f /ord-runes/target/release/ord ]; then
-    echo "Copying ord binary from image to mounted volume..."
-    cp /ord-runes/target/release_initial/ord /ord-runes/target/release/
-fi
-
 # Loop until ready.txt is found
 while [ ! -f /ready.txt ]; do
     echo "Waiting for ready.txt..."
